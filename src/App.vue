850986733db59edd66ca77c1f3524edf3b1aa893
rahulkotla2 <rahulkotla2@gmail.com>
2023-04-24 09:49:03 +0530

    Added Font Awesome Component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
 <template>
     <router-view></router-view>
 </template>
+
+<script>
+
+</script>
+
+<style>
+*{
+    padding: 0;
+    margin :0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,25 +3,43 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from './components/HomePage.vue';
 import ThresHold from './components/ThresHold.vue'
 import BaseTransform from './components/Geomteric/BaseTransform.vue'
+import BaseLoader from './components/BaseLoader.vue'
+import EditingPage from './components/EditingPage';
 import App from './App.vue'
+
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import '@fortawesome/fontawesome-free/css/all.css'
+library.add(faCoffee)
 
 const app = createApp(App);
 
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.component('base-loader', BaseLoader)
 const router = createRouter({
     history: createWebHistory(),
-    routes: [{
-        path: '/',
-        component: HomePage
-    },{
-        path : '/thresh',
-        component : ThresHold
-    },
-    {
-        path : '/geometric/:id',
-        component : BaseTransform,
-        props :true,
-    }
-]
+    routes: [
+        {
+            path: '/',
+            component: HomePage
+        },
+        {
+            path: '/Edit',
+            component: EditingPage
+        },
+        {
+            path: '/thresh',
+            component: ThresHold
+        },
+        {
+            path: '/geometric/:id',
+            component: BaseTransform,
+            props: true,
+        }
+    ]
 })
 
 app.use(router);
